f9206e362567b3e5eb7ec5c2184f47899203f2b6
florentinadaia05 <daiaflorentina05@gmail.com>
2020-10-23 15:52:36 +0300

    Modificari iarasi in Program.java

--- src/app/Program.java
+++ src/app/Program.java
@@ -6,6 +6,13 @@ public class Program {
 
 		System.out.println("Aceasta este prima versiune a programului nostru");
 		System.out.println("Am mai adaugat inca o linie de cod dupa commit-ul 1");
+		
+		System.out.println("****HELLO WORLD****");
+		System.out.println("Bine ati venit in aplicatie");
+		System.out.println("Aplicatia face tot ce doresti");
+		System.out.println("I'll grant you 3 wishes");
+		
+		System.out.println("****GOODBYE WORLD****");
 	}
 
 }
