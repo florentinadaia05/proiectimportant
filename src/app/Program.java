e2586d729d0138034e112f70ee2c57384a0e54df
florentinadaia05 <daiaflorentina05@gmail.com>
2020-10-23 15:51:49 +0300

    Am mai modificat ceva in Program.java

--- src/app/Program.java
+++ src/app/Program.java
@@ -5,6 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 
 		System.out.println("Aceasta este prima versiune a programului nostru");
+		System.out.println("Am mai adaugat inca o linie de cod dupa commit-ul 1");
 	}
 
 }
