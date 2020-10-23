4222147efec3beb7ead6fe4f870c827c03f96fe6
florentinadaia05 <daiaflorentina05@gmail.com>
2020-10-23 15:59:39 +0300

    Am sters un rand

--- src/app/Program.java
+++ src/app/Program.java
@@ -10,7 +10,6 @@ public class Program {
 		System.out.println("****HELLO WORLD****");
 		System.out.println("Bine ati venit in aplicatie");
 		System.out.println("Aplicatia face tot ce doresti");
-		System.out.println("I'll grant you 3 wishes");
 		
 		System.out.println("****GOODBYE WORLD****");
 	}
